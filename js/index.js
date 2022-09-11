3efb54550cf6fc2702b306643ef43ed4bbc8a738
Yarick126 <gadzhikurbanovy@mail.ru>
2022-09-11 07:30:45 +0300

    Сделал основу

---
+++ js/index.js
@@ -0,0 +1,18 @@
+function main (){
+
+  function toCamelCase(str){
+    let symbl = '-_';
+    for(var i = 0;i<str.lenght;i++){
+      for(var j =0;j<symbl.lenght;j++){
+        if(str[i]===symbl[j]){
+          str[i+1].toUpperCase();
+          str = str.replace(str[i],'');
+        }
+      }
+    }
+    
+    return str;
+  }
+  
+  toCamelCase("the_stealth_warrior");
+}
